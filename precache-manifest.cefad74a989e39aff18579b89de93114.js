7f2cee3b7194b1f4bc12a5916d06dc042cc75613
finer <finersanya@gmail.com>
2021-01-29 22:04:01 +0500

    1.0

---
+++ precache-manifest.cefad74a989e39aff18579b89de93114.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7f6bf3c9a50bebebca47e28557c95d4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "27c5cc29e6b49a519942",
+    "url": "./static/css/2.f53a4a22.chunk.css"
+  },
+  {
+    "revision": "e14244328549f70929d1",
+    "url": "./static/css/main.435d6662.chunk.css"
+  },
+  {
+    "revision": "27c5cc29e6b49a519942",
+    "url": "./static/js/2.c40e0ffb.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "./static/js/2.c40e0ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e14244328549f70929d1",
+    "url": "./static/js/main.c41a2c06.chunk.js"
+  },
+  {
+    "revision": "5416c08adc152c0560ee",
+    "url": "./static/js/runtime-main.b3720eee.js"
+  },
+  {
+    "revision": "ab62a6210bc1cdfed523e7c037b957c5",
+    "url": "./static/media/watermark.ab62a621.png"
+  }
+]);
