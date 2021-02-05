86b6eab23844c1d471f02f030aa2c2a97e2e86d2
finer <finersanya@gmail.com>
2021-02-05 18:26:52 +0500

    kotik init

---
+++ kotik/precache-manifest.ae7977fd2224a633436585021ac3513a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "203198a8f2df02d1686c",
+    "url": "./static/css/main.c13f7ca3.chunk.css"
+  },
+  {
+    "revision": "203198a8f2df02d1686c",
+    "url": "./static/js/main.d46e421e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "f3d1008b07380e0e2c32",
+    "url": "./static/css/2.8dd3c041.chunk.css"
+  },
+  {
+    "revision": "f3d1008b07380e0e2c32",
+    "url": "./static/js/2.8df6af8d.chunk.js"
+  },
+  {
+    "revision": "5d9233ef0fecd4c152886a736270b967",
+    "url": "./index.html"
+  }
+];
