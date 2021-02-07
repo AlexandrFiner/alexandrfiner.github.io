0e63fe6bd88584f5b8735e2003fa9f6f15f74f49
finer <finersanya@gmail.com>
2021-02-07 21:58:15 +0500

    like

---
+++ like/precache-manifest.c74f00c88373eb78e036468a578c9a39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e198fdfb97c5a646181c",
+    "url": "./static/css/main.2fae5340.chunk.css"
+  },
+  {
+    "revision": "e198fdfb97c5a646181c",
+    "url": "./static/js/main.7ebf084a.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "1227f0cf87b0981c16f5",
+    "url": "./static/css/2.fb75dcb6.chunk.css"
+  },
+  {
+    "revision": "1227f0cf87b0981c16f5",
+    "url": "./static/js/2.7a281399.chunk.js"
+  },
+  {
+    "revision": "fdbf26d0a91939360e6e1758da201632",
+    "url": "./index.html"
+  }
+];
